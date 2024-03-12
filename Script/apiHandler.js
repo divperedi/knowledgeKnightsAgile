4e625c57854be2eb8391adb4ca1c4c045d72d07e
AdreanRodriguez <loco_bladare@hotmail.com>
2024-03-12 14:17:26 +0100

    Fixade till product.html, tagit bort console.log och gjort lazyLoader.js

--- Script/apiHandler.js
+++ Script/apiHandler.js
@@ -3,7 +3,7 @@
 async function fetchData(url) {
     try {
         const response = await fetch(url);
-        const data = response.json();
+        const data = await response.json();
 
         return data;
     } catch(error) {
